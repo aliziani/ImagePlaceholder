ea737369fe4a27b68cc9d0ea6b02a9e4ded89eca
ali <zianiali89@gmail.com>
2016-01-16 21:08:48 +0100

    Add a constant file

---
+++ src/imagePlaceholder.cfg.js
@@ -0,0 +1,10 @@
+var config = {
+               loading:{
+                 imageUrl:'#',
+                 style:null,
+                 class:null
+               }
+             };
+angular
+  .module('imagePlaceholder')
+  .constant('ImgPlhdrCst',config);
